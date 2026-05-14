28ba99b68b02c038d781e9314542018d44e63d97
Khorn sopheap <sopheapkhornkh@gmail.com>
2026-05-14 19:43:18 +0700

    First initial

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  stock: Number
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
